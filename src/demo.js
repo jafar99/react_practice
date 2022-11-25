08f04ae89affd9233c299c9ba4497c742e609359
Jafar99 <jafaralihajaro@gmail.com>
2022-11-25 16:18:45 +0530

    inital commit

---
+++ src/demo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function demo() {
+  return (
+    <div>
+    
+    
+    
+    
+    
+    
+    
+    </div>
+  )
+}
+
+export default demo
---
+++ src/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+
+
+
+    <ul >
+        <li>I</li>
+        <li>me</li>
+        <li>myself</li>
+    </ul>
+
+
+
+    </div>
+   
+  )
+}
+
+export default Navbar
